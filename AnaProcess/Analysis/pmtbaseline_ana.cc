f7e34c5f2618a2497dce8f5cb847a17a6630e7bd
kazuhiro <kazuhiro@nevis.columbia.edu>
2013-08-07 13:30:05 -0400

    Allowing cuts in pmtbaseline_ana -- kazu

--- AnaProcess/Analysis/pmtbaseline_ana.cc
+++ AnaProcess/Analysis/pmtbaseline_ana.cc
@@ -3,6 +3,31 @@
 
 #include "pmtbaseline_ana.hh"
 
+pmtbaseline_ana::pmtbaseline_ana(){
+
+  _name="pmtbaseline_ana"; 
+  _fout=0;
+  reset_cuts();
+
+}
+
+void pmtbaseline_ana::reset_cuts() {
+
+  _cut_tstart_reco = std::make_pair(-1,2000);
+  _cut_tstart   = std::make_pair(-1,2000);
+  _cut_tend     = std::make_pair(-1,2000);
+  _cut_amp      = std::make_pair(-1,4096);
+  _cut_charge   = std::make_pair(-1,4096*2000);
+  _cut_pedbase  = std::make_pair(-1,4096);
+  _cut_pedrms   = std::make_pair(0,4096);
+  _cut_channels = std::make_pair(0,PMT::INVALID_CH);
+  _cut_event_id = std::make_pair(0,0xffffffff);
+  _cut_npulse   = std::make_pair(0,0xffffffff);
+  _cut_sum_charge = std::make_pair(0,4096*2000*PMT::NUM_PMT_CHANNEL);
+  _cut_sum_peak = std::make_pair(0,4096*PMT::NUM_PMT_CHANNEL);
+  
+}
+
 bool pmtbaseline_ana::initialize() {
 
   //
@@ -32,13 +57,53 @@ bool pmtbaseline_ana::analyze(storage_manager* storage) {
   //std::cout << Form("Event ID: %d",event_wf->event_id()) << std::endl;
   //
 
-  // Get a pointer to pulse collection object
+  const event_waveform* wfs = (event_waveform*)(storage->get_data(DATA_STRUCT::WF_COLLECTION));
   const pulse_collection* pulses = (pulse_collection*)(storage->get_data(DATA_STRUCT::PULSE_COLLECTION));
+
+  PMT::word_t event_id = wfs->event_id();
+  double sum_charge    = pulses->sum_charge();
+  double sum_peak      = pulses->sum_peak();
+  uint32_t npulse      = pulses->npulse();
+
+  // Check if this event is in the range of users' interest
+  if(event_id < _cut_event_id.first || _cut_event_id.second < event_id)
+    return true;
+  else if(sum_charge < _cut_sum_charge.first || _cut_sum_charge.second < sum_charge)
+    return true;
+  else if(sum_peak < _cut_sum_peak.first || _cut_sum_peak.second < sum_peak)
+    return true;
+  else if(npulse < _cut_npulse.first || _cut_npulse.second < npulse)
+    return true;
 
   // Loop over pulse collection and fill histograms
   for(pulse_collection::const_iterator iter(pulses->begin()); iter!=pulses->end(); ++iter){
 
-    PMT::ch_number_t ch=(*iter).channel_number();
+    PMT::ch_number_t ch((*iter).channel_number());
+    double      t_start  = (*iter).start_time();
+    double      t_end    = (*iter).end_time();
+    double      charge   = (*iter).charge();
+    double      amp      = (*iter).pulse_peak();
+    double      ped_base = (*iter).ped_mean();
+    double      ped_rms  = (*iter).ped_rms();
+    double      t_start_reco = (*iter).start_time_reco();
+
+    // Check if this pulse passes the criteria
+    if(ch < _cut_channels.first || _cut_channels.second < ch)
+      continue;
+    if(t_start_reco < _cut_tstart_reco.first || _cut_tstart_reco.second < t_start_reco)
+      continue;
+    if(t_start < _cut_tstart.first || _cut_tstart.second < t_start)
+      continue;
+    if(t_end < _cut_tend.first || _cut_tend.second < t_end)
+      continue;
+    if(charge < _cut_charge.first || _cut_charge.second < charge)
+      continue;
+    if(amp < _cut_amp.first || _cut_amp.second < amp)
+      continue;
+    if(ped_base < _cut_pedbase.first || _cut_pedbase.second < ped_base)
+      continue;
+    if(ped_rms < _cut_pedrms.first || _cut_pedrms.second < ped_rms)
+      continue;    
 
     // Check if a histogram of this pulse's channel already exists. If not, add.
     if(_hArray_PedMean.find(ch) == _hArray_PedMean.end()) {
@@ -51,17 +116,17 @@ bool pmtbaseline_ana::analyze(storage_manager* storage) {
 				     50,0,10);
       _hArray_Charge[ch]  = new TH1D(Form("hCharge_Ch%03d",ch),
 				     Form("Integrated Charge Sum for Ch %03d; Charge [arbitrary];Entries",ch),
-				     600,0,3000);
+				     600,0,600);
       _hArray_Peak[ch]    = new TH1D(Form("hPeak_Ch%03d",ch),
 				     Form("Max. Amplitude for Ch %03d; Peak Amp. [ADC]; Entries",ch),
-				     500,0,500);
+				     500,0,200);
     }
     
     // Fill histograms
-    _hArray_PedMean[ch]->Fill((*iter).ped_mean());
-    _hArray_PedRMS[ch]->Fill((*iter).ped_rms());
-    _hArray_Charge[ch]->Fill((*iter).charge());
-    _hArray_Peak[ch]->Fill((*iter).pulse_peak());
+    _hArray_PedMean[ch]->Fill(ped_base);
+    _hArray_PedRMS[ch]->Fill(ped_rms);
+    _hArray_Charge[ch]->Fill(charge);
+    _hArray_Peak[ch]->Fill(amp);
     
   }
 
--- AnaProcess/Analysis/pmtbaseline_ana.hh
+++ AnaProcess/Analysis/pmtbaseline_ana.hh
@@ -28,7 +28,7 @@ class pmtbaseline_ana : public ana_base{
 public:
 
   /// Default constructor
-  pmtbaseline_ana(){ _name="pmtbaseline_ana"; _fout=0;};
+  pmtbaseline_ana();
 
   /// Default destructor
   virtual ~pmtbaseline_ana(){};
@@ -48,9 +48,103 @@ public:
   */
   virtual bool finalize();
 
+  /// Setter for the range of pusle start time to select pulses of interest
+  void set_range_pulse_start_time(double v1, double v2)   {_cut_tstart.first=v1;   _cut_tstart.second=v2;   };
+
+  /// Setter for the range of reconstructed pusle start time to select pulses of interest
+  void set_range_pulse_start_time_reco(double v1, double v2)   {_cut_tstart_reco.first=v1;   _cut_tstart_reco.second=v2;   };
+
+  /// Setter for the range of pusle end time to select pulses of interest..
+  void set_range_pulse_end_time(double v1, double v2)     {_cut_tend.first=v1;     _cut_tend.second=v2;     };
+
+  /// Setter for the range of pusle amplitude to select pulses of interest..
+  void set_range_pulse_amp(double v1, double v2)          {_cut_amp.first=v1;      _cut_amp.second=v2;      };
+
+  /// Setter for the range of integrated charge to select pulses of interest..
+  void set_range_pulse_charge(double v1,double v2)        {_cut_charge.first=v1;   _cut_charge.second=v2;   };
+
+  /// Setter for the range of pedestal mean value to select pulses of interest..
+  void set_range_ped_mean(double v1, double v2)           {_cut_pedbase.first=v1;  _cut_pedbase.second=v2;  };
+
+  /// Setter for the range of pedestal rms to select pulses of interest..
+  void set_range_ped_rms(double v1, double v2)            {_cut_pedrms.first=v1;   _cut_pedrms.second=v2;   };
+
+  /// Setter for the range of event id to select pulses of interest.
+  void set_range_event_id(PMT::word_t v1, PMT::word_t v2) {_cut_event_id.first=v1; _cut_event_id.second=v2; };
+
+  /// Setter for the range of channel numbers to select pulses of interest.
+  void set_range_channel(PMT::ch_number_t v1, PMT::ch_number_t v2) {_cut_channels.first=v1; _cut_channels.second=v2;};
+
+  /// Setter for the range of event charge sum to select pulses of interest.
+  void set_range_sum_charge(double v1, double v2)         {_cut_sum_charge.first=v1; _cut_sum_charge.second=v2;};
+
+  /// Setter for the range of event peak sum to select pulses of interest.
+  void set_range_sum_peak(double v1, double v2)           {_cut_sum_peak.first=v1; _cut_sum_peak.second=v2;};
+
+  /// Setter for the range of event-wise number of pulses to select pulses of interest.
+  void set_range_npulse(double v1, double v2)             {_cut_npulse.first=v1; _cut_npulse.second=v2;};
+
+  //
+  // Getters
+  //
+
+  /// Getter for the range of pusle start time to select pulses of interest
+  const std::pair<double,double> range_pulse_start_time() const   {return _cut_tstart;};
+
+  /// Getter for the range of pusle reconstructed start time to select pulses of interest
+  const std::pair<double,double> range_pulse_start_time_reco() const   {return _cut_tstart_reco;};
+
+  /// Getter for the range of pusle end time to select pulses of interest..
+  const std::pair<double,double> range_pulse_end_time() const     {return _cut_tend;};
+
+  /// Getter for the range of pusle amplitude to select pulses of interest..
+  const std::pair<double,double> range_pulse_amp() const          {return _cut_amp;};
+
+  /// Getter for the range of integrated charge to select pulses of interest..
+  const std::pair<double,double> range_pulse_charge() const       {return _cut_charge;};
+
+  /// Getter for the range of pedestal mean value to select pulses of interest..
+  const std::pair<double,double> range_ped_mean() const           {return _cut_pedbase;};
+
+  /// Getter for the range of pedestal rms to select pulses of interest..
+  const std::pair<double,double> range_ped_rms() const            {return _cut_pedrms;};
+
+  /// Getter for the range of event id to select pulses of interest.
+  const std::pair<PMT::word_t,PMT::word_t> range_event_id() const {return _cut_event_id;};
+
+  /// Getter for the range of channel numbers to select pulses of interest.
+  const std::pair<PMT::word_t,PMT::word_t> range_channel() const  {return _cut_channels;};
+
+  /// Getter for the range of event charge sum to select pulses of interest.
+  const std::pair<double,double> range_sum_charge() const         {return _cut_sum_charge;};
+
+  /// Getter for the range of event peak sum to select pulses of interest.
+  const std::pair<double,double> range_sum_peak() const           {return _cut_sum_peak;};
+
+  /// Getter for the range of event-wise number of pulses to select pulses of interest.
+  const std::pair<uint32_t,uint32_t> range_npulse() const         {return _cut_npulse;};
+
 protected:
 
+  /// reset cut values
+  void reset_cuts();
+
   std::map<PMT::ch_number_t,TH1D*> _hArray_PedMean, _hArray_PedRMS, _hArray_Charge, _hArray_Peak;
+
+  /// pairs to define a range of pulse parameters for displaying pulses.
+  std::pair<double,double> _cut_tstart, _cut_tstart_reco, _cut_tend, _cut_amp, _cut_charge, _cut_pedbase, _cut_pedrms;
+
+  /// pairs to define a range of event parameters for displaying pulses.
+  std::pair<double,double> _cut_sum_charge, _cut_sum_peak;
+
+  /// pairs to define a range of event parameters for displaying pulses.
+  std::pair<uint32_t,uint32_t> _cut_npulse;  
+
+  /// a pair to define a range of event id for displaying pulses.
+  std::pair<PMT::word_t,PMT::word_t> _cut_event_id;
+
+  /// a pair to define a range of channel numbers for displaying pulses.
+  std::pair<PMT::ch_number_t,PMT::ch_number_t> _cut_channels;
 
 };
 
