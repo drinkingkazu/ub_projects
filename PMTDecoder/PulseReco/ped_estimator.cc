1118ba3c21e640228672bd4e29040709574e0a7b
kazuhiro <kazuhiro@nevis.columbia.edu>
2013-09-17 13:44:59 -0400

    Bug fix on ped estimation method --kazu

--- PMTDecoder/PulseReco/ped_estimator.cc
+++ PMTDecoder/PulseReco/ped_estimator.cc
@@ -18,17 +18,26 @@ void ped_estimator::analyze_head(const std::vector<uint16_t> *wf){
   _mean_head = 0;
   _rms_head  = 0;
 
-  for(size_t index=0; index < _nsample_head; ++index)
+  size_t max_sample = (_nsample_head < wf->size() ? _nsample_head : wf->size());
+  
+  if(max_sample < _nsample_head)
+
+    Message::send(MSG::WARNING,__FUNCTION__,
+		  Form("Detected a waveform of length %zu ... shortened pedestal sample!",
+		       wf->size())
+		  );
+  
+  for(size_t index=0; index < max_sample; ++index)
 
     _mean_head += wf->at(index);
 
-  _mean_head = _mean_head / (double)(_nsample_head);
+  _mean_head = _mean_head / (double)(max_sample);
 
-  for(size_t index=0; index < _nsample_head; ++index)
+  for(size_t index=0; index < max_sample; ++index)
 
     _rms_head += pow( (wf->at(index) - _mean_head), 2 );
 
-  _rms_head = sqrt(_rms_head/((double)(_nsample_head)));
+  _rms_head = sqrt(_rms_head/((double)(max_sample)));
 
 }
 
@@ -37,17 +46,26 @@ void ped_estimator::analyze_tail(const std::vector<uint16_t> *wf){
   _mean_tail = 0;
   _rms_tail  = 0;
 
-  for(size_t index=wf->size()-1; index > (wf->size()-1-_nsample_tail); --index)
+  size_t max_sample = (_nsample_tail < wf->size() ? _nsample_tail : wf->size());
+
+  if(max_sample < _nsample_head)
+
+    Message::send(MSG::WARNING,__FUNCTION__,
+		  Form("Detected a waveform of length %zu ... shortened pedestal sample!",
+		       wf->size())
+		  );
+
+  for(size_t index=wf->size()-1; index > (wf->size()-1-max_sample); --index)
 
     _mean_tail += wf->at(index);
 
-  _mean_tail = _mean_tail / (double)(_nsample_tail);
+  _mean_tail = _mean_tail / (double)(max_sample);
 
-  for(size_t index=wf->size()-1; index > ( (wf->size()) - 1 - _nsample_tail); --index)
+  for(size_t index=wf->size()-1; index > ( (wf->size()) - 1 - max_sample); --index)
 
     _rms_tail += pow( (wf->at(index) - _mean_tail), 2 );
 
-  _rms_tail = sqrt(_rms_tail/((double)(_nsample_tail)));
+  _rms_tail = sqrt(_rms_tail/((double)(max_sample)));
 
 }
 
